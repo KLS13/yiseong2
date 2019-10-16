b09ff4f54b3f98aa3501b8269d1fbfd502b2c7c8
Kang Yi Seong <54849894+KLS13@users.noreply.github.com>
2019-10-16 21:35:43 +0900

    자바스크립트( ES6 ) 3주차

---
+++ JavaScript week3/2019.10.16/110.promise.js
@@ -0,0 +1,11 @@
+const promiseResult = new Promise((resolve) => {
+    resolve(1); // 핵심은 첫번째는 resolve() 를 쓴다는 것.
+}).then((result) => {
+    console.log('first:', result);
+    return `${result}hello`;
+}).then((result) => {
+    console.log('second:', result);
+    return `${result}nello`;
+});
+
+promiseResult.then(result => console.log(result));
