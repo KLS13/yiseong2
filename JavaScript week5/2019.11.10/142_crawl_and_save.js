aa559dad3b3bee8a76d0a653f7d3f1d47d1f9a1b
Kang Yi Seong <54849894+KLS13@users.noreply.github.com>
2019-11-10 23:07:35 +0900

    자바스크립트( ES6 ) 5주차

---
+++ JavaScript week5/2019.11.10/142_crawl_and_save.js
@@ -0,0 +1,16 @@
+const crawler = require('crawler');
+const cheerio = require('cheerio');
+const fs = require('fs');
+
+const parse = (decodedResult) => {
+  const $ = cheerio.load(decodedResult);
+  const titles = $('h3.r').find('a');
+  titles.each((index, element) => {
+    const title = $(element).text();
+    console.log(title);
+    fs.appendFile('./titles.txt', `${title}\n`);
+  });
+};
+
+crawler.crawl(parse)({ q: '맛집' });
+
