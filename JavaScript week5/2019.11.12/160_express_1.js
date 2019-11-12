e4d6a7b0635fb3be4b9c145b8f55ee63ffafbb1f
Kang Yi Seong <54849894+KLS13@users.noreply.github.com>
2019-11-12 22:18:56 +0900

    자바스크립트( ES6 ) 5주차

---
+++ JavaScript week5/2019.11.12/160_express_1.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/',(request, response) => {
+  response.send('Hello express module');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running port 3000!');
+  console.log('http://127.0.0.1:3000');
+});
